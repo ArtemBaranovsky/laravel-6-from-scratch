d1b830bef26d5277297bd3973892713164ed5218
Artem  Baranovskyi <artem.baranovsky1980@gmail.com>
2020-10-21 17:03:42 +0300

    07 Route Wildcards

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,16 @@
 |
 */
 
-Route::get('/', function () {
-    $name = request('name');
-    return view('welcome', [
-        'name' => $name
+Route::get('/post/{post}', function ($post) {
+    $posts = [
+        'my-first-post' => 'Hello, this is my first blog post!',
+        'my-second-post' => 'Now I am getting the hang of this blogging thing.'
+    ];
+    if (!array_key_exists($post, $posts)) {
+        abort(404, 'Sorry that post was not found.');
+    }
+    return view('post', [
+//        'post' => $posts[$post] ?? 'Nothing here yet.'
+        'post' => $posts[$post]
     ]);
 });
