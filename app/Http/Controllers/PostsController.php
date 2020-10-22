0f53f9ec4de7fd9c8da5b98c69b28deda48a84da
Artem Baranovsky <artem.baranovsky1980@gmail.com>
2020-10-22 17:24:01 +0300

    12 Generate Multiple Files in a Single Command

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -13,13 +13,6 @@ class PostsController  extends Controller
      */
     public function show($slug)
     {
-//        $post = \DB::table('posts')->where('slug', $slug)->first();
-//        $post = Post::where('slug', $slug)->first();
-//        $post = Post::where('slug', $slug)->firstOrFail();
-
-        //        if (!$post) {
-//            abort(404, 'Sorry that post was not found.');
-//        }
         return view('post', [
             'post' => Post::where('slug', $slug)->firstOrFail()
         ]);
