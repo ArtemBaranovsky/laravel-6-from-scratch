5cdb74cda4e24e293a09155e916d354e187f20a4
Artem Baranovsky <artem.baranovsky1980@gmail.com>
2020-10-22 15:19:25 +0300

    11 Migrations 101

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,6 +3,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 
 class PostsController  extends Controller
 {
@@ -10,23 +11,17 @@ class PostsController  extends Controller
      * @param $post
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-//    public function show($post)
     public function show($slug)
     {
-//        dd(env('DB_CONNECTION'));
-//        $posts = [
-//            'my-first-post' => 'Hello, this is my first blog post!',
-//            'my-second-post' => 'Now I am getting the hang of this blogging thing.'
-//        ];
+//        $post = \DB::table('posts')->where('slug', $slug)->first();
+//        $post = Post::where('slug', $slug)->first();
+//        $post = Post::where('slug', $slug)->firstOrFail();
 
-        $post = \DB::table('posts')->where('slug', $slug)->first();
-//        dd($post);
-//        if (!array_key_exists($post, $posts)) {
+        //        if (!$post) {
 //            abort(404, 'Sorry that post was not found.');
 //        }
         return view('post', [
-//            'post' => $posts[$post]
-            'post' => $post
+            'post' => Post::where('slug', $slug)->firstOrFail()
         ]);
     }
 }
