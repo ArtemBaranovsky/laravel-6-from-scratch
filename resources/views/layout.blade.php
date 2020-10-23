34b0a7c0dee5749d9457d9ceb3d201a87be305d9
Artem Baranovsky <artem.baranovsky1980@gmail.com>
2020-10-23 17:20:23 +0300

    17 Asset Compilation with Laravel Mix and webpack

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -19,6 +19,8 @@ Released   : 20140225
     <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
     <link href="css/default.css" rel="stylesheet" />
     <link href="css/fonts.css" rel="stylesheet" />
+{{--    <link rel="stylesheet" href="/css/app.css">--}}
+    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
 
 </head>
 <body>
@@ -29,11 +31,14 @@ Released   : 20140225
             </div>
             <div id="menu">
                 <ul>
-                    <li class="current_page_item"><a href="#" accesskey="1" title="">Homepage</a></li>
-                    <li><a href="#" accesskey="2" title="">Our Clients</a></li>
-                    <li><a href="/about" accesskey="3" title="">About Us</a></li>
-                    <li><a href="#" accesskey="4" title="">Careers</a></li>
-                    <li><a href="#" accesskey="5" title="">Contact Us</a></li>
+{{--                    <li class="current_page_item"><a href="#" accesskey="1" title="">Homepage</a></li>--}}
+                    <li class="{{ Request::path() === '/' ? 'current_page_item' : '' }}"><a href="/" accesskey="1" title="">Homepage</a></li>
+                    <li class="{{ Request::path() === 'clients' ? 'current_page_item' : '' }}"><a href="/clients" accesskey="2" title="">Our Clients</a></li>
+                    <li class="{{ Request::path() === 'about' ? 'current_page_item' : '' }}"><a href="/about" accesskey="3" title="">About Us</a></li>
+{{--                    <li class="{{ Request::is() === 'about' ? 'current_page_item' : '' }}"><a href="/about" accesskey="3" title="">About Us</a></li>--}}
+{{--                    <li class="{{ Request::is() === 'about*' ? 'current_page_item' : '' }}"><a href="/about" accesskey="3" title="">About Us</a></li>--}}{{--using a wildcard--}}
+                    <li class="{{ Request::path() === 'careers' ? 'current_page_item' : '' }}"><a href="/careers" accesskey="4" title="">Careers</a></li>
+                    <li class="{{ Request::path() === 'contact' ? 'current_page_item' : '' }}"><a href="/contact" accesskey="5" title="">Contact Us</a></li>
                 </ul>
             </div>
         </div>
@@ -41,6 +46,10 @@ Released   : 20140225
     </div>
 
     @yield('content')
+    <div id="copyright" class="container">
+        <p>&copy; Untitled. All rights reserved. | Photos by <a href="http://fotogrph.com/">Fotogrph</a> | Design by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>.</p>
+    </div>
+    <script src="/js/app.js"></script>
 </body>
 </html>
 {{--@yield('content')--}}
