78c6d17b3c79d0f78e68c8d3e2e12ad1ff8d9984
Artem Baranovsky <artem.baranovsky1980@gmail.com>
2020-11-13 11:27:54 +0200

    49 Eventing Pros and Cons

--- app/Http/Controllers/PaymentsController.php
+++ app/Http/Controllers/PaymentsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\ProductPurchased;
 use App\Notifications\PaymentReceived;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Notification;
@@ -15,6 +16,20 @@ class PaymentsController extends Controller
 
     public function store()
     {
+        #core effects
+        // process the payment using some billing provider
+        // unlock the purchase
+
+        // ProductPurchased event
+        ProductPurchased::dispatch('toy');
+//        event(new ProductPurchased('toy'));   // dispatching this event with a helper
+
+        #side effects
+        // listeners:
+        // notify the user about the payment
+        // award achievements   +
+        // send shareable coupon to user +
+
 //        Mail::to();
 //        Notification::send(request()->user());
 //        Notification::send(request()->user(), new PaymentReceived());
